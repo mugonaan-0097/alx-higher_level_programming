72eb5751e67dc0892758f5d45980e71f56c346ef
mugonaan-0097 <mugonaan@gmail.com>
2022-11-28 15:10:38 -0800

    smash

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print("First three letters: {}".format(word_first_3))
-print("Last two letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Secu
+str1 = "Holberton"
+str2 = "School"
+str1 += " " + str2
+print(f"Welcome to {}!".format(str1))
