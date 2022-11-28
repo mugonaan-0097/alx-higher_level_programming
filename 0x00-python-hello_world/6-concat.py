6f75024bbfd6f82c40c2345aac795e9db40f88d1
mugonaan-0097 <mugonaan@gmail.com>
2022-11-28 14:40:32 -0800

    concat

---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word word[1:-1]
+print("First three laters: {}".format(word_first_3))
+print("Last two latters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Secu
