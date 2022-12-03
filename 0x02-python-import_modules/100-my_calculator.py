c94e0543b5ccf32c3f667f62ae21372cfa364fb5
mugonaan-0097 <mugonaan@gmail.com>
2022-12-03 11:19:38 -0800

    100 and 101

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-
+from sys import argv
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(ops.keys()):
-            print("Unkown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        exit(1)
+    if argv[2] == "+":
+        print("{:s} + {:s} = {:d}"
+                .format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "-":
+        print("{:s} - {:s} = {:d}"
+                .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "*":
+        print("{:s} * {:s} = {:d}"
+                .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "/":
+        print("{:s} / {:s} = {:d}"
+                .format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+    else:
+        print("Unknown operator. Available operator: +, -, * and/")
+        exit(1)
