1cba1167eac64993cbb9146b742f8fb4cf7481ca
mugonaan-0097 <mugonaan@gmail.com>
2022-12-08 13:28:31 -0800

    best

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-        new_list = []
-        for i in my_list:
-            if i not in new_list:
-                new_list.append(i)
-        return sum(new_list)
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+        return (num)
