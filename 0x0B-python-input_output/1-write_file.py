8d4130a514ce3ac82cd81deb5a0b6fbaad306a0c
mugonaan-0097 <mugonaan@gmail.com>
2023-01-22 06:59:13 -0800

    move

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # 1-number_of_lines.py
 # Danladi Mugonaan Daloek <@Alx.com>
-"""Defines a text file characters numbers function."""
+"""Defines a text file characters number function."""
 
 
 def write_file(filename="", text=""):
     """Return the number of characters in a text file."""
-    lines = 0
+    characters = 0
     with open(filename) as f:
-        for line in f:
-            lines += 1
-        return lines
+        for character in f:
+            characters += 1
+        return characters
