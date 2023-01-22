c003a6980170216a0e9a3815d2e69c46e0f98523
mugonaan-0097 <mugonaan@gmail.com>
2023-01-22 07:08:20 -0800

    neves

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,8 +6,8 @@
 
 def write_file(filename="", text=""):
     """Return the number of characters in a text file."""
-    characters = 0
+    nb_characters = 0
     with open(filename) as f:
-        for character in f:
-            characters += 1
-        return characters
+        for nb_character in f:
+            nb_characters += 1
+        return nb_characters
